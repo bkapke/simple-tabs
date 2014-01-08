b9ebadf68596b44902099e6caf25c884468691fe
Brian Kapke <brnkapke@gmail.com>
2014-01-08 01:23:09 -0600

    first commit ever

---
+++ readme.md
@@ -0,0 +1,1 @@
+#A Simple Tab Library#
---
+++ simple-tabs.js
@@ -0,0 +1,40 @@
+var SimpleTabs = function (elem) {
+    //get tab objects and store as pane + tab
+    var activeTabObject;
+    
+    var TabObject = function () {
+        var self = this;
+        this.tab; //element
+        this.pane; //element
+        this.setClick = function () {
+            $(self.tab).click(function () {
+                self.showThisTab();
+            });
+        };
+        
+        this.showThisTab = function () {
+            if (self !== activeTabObject) {
+                //change the tab page and update the active tab
+                $(activeTabObject.pane).removeClass('active-page');
+                $(activeTabObject.tab).removeClass('active');
+                $(self.pane).addClass('active-page');
+                $(self.tab).addClass('active')
+                activeTabObject = self;
+            }
+        };
+        
+    };
+    
+    $.each(elem.children(), function (id,val){
+        var tab = new TabObject();
+        tab.tab = val;
+        var classString = $(val).attr('class')
+        var className = classString.split(' ')[0];
+        tab.pane = $('#' + className);
+        tab.setClick();
+        if (classString.indexOf('active') > -1) {
+            activeTabObject = tab;
+        }
+    })
+    
+}
